40f697eb4f97f89a94ec5ca7c6b7ae4e7337942a
Rossy Lopez <rossy181071@hotmail.com>
2026-04-14 10:19:36 -0600

    Initial commit

--- src/scooter-component.js
+++ src/scooter-component.js
@@ -58,20 +58,20 @@ export class ScooterComponent extends LitElement {
        this.paragraph1 = "Contamos con Una amplia gama en Scooter El Xiaomi Electric Scooter 4 Pro 2ndGen es un patinete eléctrico que llama la atención por varias razones, siendo una de ellas que está orientado tanto a usuarios avanzados como a aquellos que quieran dar el salto a este tipo de dispositivos"
 
        this.title2 = "Scooter Honey Whall"
-       this.imgUrl2 = "https://honeywhale.com.mx/wp-content/uploads/2024/06/Honeywhale-scooter-M5-Pro-1-1020x1020.webp"
+       this.imgUrl2 = "https://tse4.mm.bing.net/th/id/OIP.rp5Y4DLVJuYVy2zgne1bFQHaHa?pid=Api&P=0&h=180"
        this.paragraph2 = "El nuevo scooter eléctrico Honey Whale M5 Pro-2 , nuevas llantas, más anchas y gruesas. Utiliza un motor mejorado, rendimiento estable, gran potencia, diseño impermeable, sin miedo a la lluvia."
 
        this.title3 = "Scooter Nanofort plegable"
-       this.imgUrl3 = "https://nanofort.com.mx/cdn/shop/products/1_1234b54d-b2f5-4828-83b2-905250742af6.jpg?v=1670885453"
+       this.imgUrl3 = "https://www.yankodesign.com/images/design_news/2024/10/foldable-e-scooter-for-last-mile-travel-within-city-limits-is-a-futureproof-nano-mobility-solution/Ant-Formica-Foldable-Scooter-2.jpg"
        this.paragraph3 ="CARRITO TIPO SCOOTER. La silla de ruedas cuenta con un sistema eléctrico de 250W para funcionar, alcanza una velocidad de 8 km/h y puede dar marcha atrás hasta los 4 km/h con un sonido de advertencia cuando se coloca la reversa"
 
        this.title4 = "Scooter Sangkee Infantil"
-       this.imgUrl4 = "https://sangkee.mx/wp-content/uploads/2021/10/Scooter-Ninos-Multifuncional-Con-Asiento-Y-Mango-Ajustable1.webp"
-       this.paragraph4 = "Scooter ideal para ni;os con bonitos colores y mucha seguridad"
+       this.imgUrl4 = "https://sdmed.cl/wp-content/uploads/2023/06/1-todos.jpg"
+       this.paragraph4 = "Scooter ideal para niños con bonitos colores y mucha seguridad"
 
        this.title5 ="Scooter la Viga"
        this.imgUrl5 = "https://ss201.liverpool.com.mx/xl/1122655136_7p.jpg"
-       this.paragraph5 = "Ideal para ni&a con bonito e idel color y muy sofisticado"
+       this.paragraph5 = "Ideal para niña con bonito e idel color y muy sofisticado"
 
 
     }
@@ -103,8 +103,7 @@ export class ScooterComponent extends LitElement {
         <img class="img-scooter" src="${this.imgUrl4}">
         <p>${this.paragraph4}</p>
 
-        
-        
+                
         <li><h1>${this.title5}</h1></li>
         <img class="img-scooter" src="${this.imgUrl5}">
         <p>${this.paragraph5}</p>
